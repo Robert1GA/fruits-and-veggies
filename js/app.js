b239d9cfb280ca48b051733188751de33a9cc97a
Robert1GA <roberts.2015@yahoo.co.uk>
2017-01-18 08:45:45 -0800

    complete, without win notification and some formulas can be combined

--- js/app.js
+++ js/app.js
@@ -1,6 +1,51 @@
 /* setup your angular app here */
+var app = angular.module("FoodApp",[]);
+app.controller('FoodCtrl',['$scope', function($scope) {
+  $scope.foods = fruits.concat(vegetables);
+  $scope.foods.sort(function(a, b) {
+    return 0.5 - Math.random()
+  });
 
-// debug stuff to show the app is loading and fruit / veggies are available
+  $scope.fruits = [];
+  $scope.veggies = [];
+
+
+  $scope.moveToFruit = function(foodIndex) {
+    $scope.fruits.push($scope.foods[foodIndex]);
+    $scope.foods.splice(foodIndex,1);
+  }
+
+  $scope.moveToVeggie = function(foodIndex) {
+    $scope.veggies.push($scope.foods[foodIndex]);
+    $scope.foods.splice(foodIndex,1);
+  }
+
+  $scope.moveToFood = function(foodType, index) {
+    console.log(foodType);
+    $scope.foods.push($scope[foodType][index]);
+    $scope[foodType].splice(index,1);
+  }
+
+  $scope.checkCorrect = function(foodType, currentSelection) {
+    if (foodType == 'fruit') {
+      if (fruits.indexOf(currentSelection) === -1) {
+        return true;
+      } else if (fruits.indexOf(currentSelection) >= 0) {
+        return false;
+      }
+    } else if (foodType == 'veggie') {
+      if (vegetables.indexOf(currentSelection) === -1) {
+        return true;
+      } else if (vegetables.indexOf(currentSelection) >= 0) {
+        return false;
+      }
+    }
+  }
+
+}])
+
+
+// debug stuff to show the app is loading and fruit // veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
